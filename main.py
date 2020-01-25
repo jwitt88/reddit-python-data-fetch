9ed23d29eb4b445b689558100aac275224b68e9a
Clyde W <59844570+jwitt88@users.noreply.github.com>
2020-01-25 02:22:41 -0600

    Updates to main.py
    - Added comment lines to provide additional context / explanation for code.
    - Added a try conditional for receiving search results.
    - Converted argv params to be passed after running the code.
    - Changed output file to now only depend on the subreddit name. (sub-results.csv)

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
-from sys import argv
 import time
 
 import praw
-import config
+import config #contains client_id, client_secret, and user_agent
 
-script, target_sub, target_search, days = argv
 
-def query_reddit(target_sub, target_search, date_cutoff):
+def query_reddit(target_sub, target_search, target_count):
     """Fetches Reddit data with praw / config credentials."""
 
-    results = reddit.subreddit(target_sub).search(\
-    target_search, sort='new', limit=50)
+    try:
+        results = reddit.subreddit(target_sub).search(\
+        target_search, sort='new', limit=target_count)
 
-    filter_results(results)
+    except:
+        print(f"\n  (X) Authentication failed - check credentials!\n")
+
+    else:
+        print("\n  [POST]   [TITLE]")
+        filter_results(results)
 
 def filter_results(results):
     """Filters the results from query_reddit() and appends to push_list"""
 
     for post in results:
 
-        if post.created_utc > date_cutoff:
+        formatted_title = post.title.replace(',', '')
+        print(f"  {post.id} | {formatted_title[:40]} (...)")
 
-            formatted_title = post.title.replace(',', '')
-            print(f"  {post.id} | {formatted_title[:40]} (...)")
+        '''
+        TO DO:
+        - Do something with the below list of comment ids.
+        - Consider storing it in the same output, or make a new file:
+            for comment in comment_ids:
+                print(comment.body) ...
+        '''
 
-            '''
-            TO DO:
-            - Do something with the below list of comment ids.
-            - Consider storing it in the same output, or make a new file:
-                for comment in comment_ids:
-                    print(comment.body) ...
-            '''
+        comment_ids = post.comments.list()
+        comment_count = len(comment_ids)
 
-            comment_ids = post.comments.list()
-            comment_count = len(comment_ids)
-
-            submission_time = convert_from_utc(post.created_utc)
-            post_data = f"{post.id}, {post.score}, {comment_count}, {submission_time}, {formatted_title}"
-            push_list.append(post_data)
-
-        else:
-            pass
+        submission_time = convert_from_utc(post.created_utc)
+        post_data = f"{post.id},{post.score},{comment_count},{submission_time},{formatted_title}"
+        push_list.append(post_data)
 
 def write_to_file(stuff_to_send):
     """Checking for existing files / data, and writing fetched content."""
@@ -58,14 +57,14 @@ def write_to_file(stuff_to_send):
                     characters of each line (the post ID) to avoid dupes.
                     """
                     if item[:6] not in existing_contents:
-                        f.write(f"{item} \n")
+                        f.write(f"{item}\n")
 
                 print(f"\n  (+) Updating {file_name} w/ fetched content.")
                 print(f"      Write successfully completed.\n")
 
         except FileNotFoundError:
             with open(file_name, 'w') as f:
-                f.write(f"Post ID, Score, Comments, Date, Title \n")
+                f.write(f"Post ID,Score,Comments,Date,Title\n")
                 for item in stuff_to_send:
                     f.write(f"{item} \n")
 
@@ -86,19 +85,22 @@ def convert_from_utc(utc_time):
     return fixed_time
 
 
+#params for querying with praw
+sub_to_search = input("\n  > Subreddit: ")
+terms_to_search = input("  > Search terms: ")
+count_to_pull = int(input("  > Pull count: "))
+
+#authentication with praw
 reddit = praw.Reddit(client_id=config.cid,
 client_secret=config.cs, user_agent=config.ua)
 
+#used in time conversion from unix
 current_time = time.time()
-date_cutoff = current_time - int(days) * 86400
 
-file_name = f"{target_sub}-{target_search}.csv"
+#file name for export and push list
+file_name = f"{sub_to_search}-results.csv"
 push_list = []
 
-print(f"\n  SUBREDDIT\n  r/{target_sub}")
-print(f"\n  KEYWORD\n  '{target_search}'")
-print(f"\n  DATE RANGE\n  < {days} day(s)")
-print(f"\n  RESULTS: ")
-
-query_reddit(target_sub, target_search, date_cutoff)
+#runs the query and writes to output
+query_reddit(sub_to_search, terms_to_search, count_to_pull)
 write_to_file(push_list)
